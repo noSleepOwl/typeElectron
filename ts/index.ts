d73e6d00a277739eab51b609c355067bdd71e662
史恭文 <844910815@qq.com>
2018-01-31 12:26:24 +0800

    提交

--- ts/index.ts
+++ ts/index.ts
@@ -4,12 +4,9 @@ import { remote } from 'electron';
 const { BrowserWindow } = remote;
 window.$ = window.jQuery = $;
 import 'bootstrap';
-namespace html {
-    export let button = `<button class="btn btn-info btn-lg">测试用的按钮</button>`;
-    export let li = `<li class="list-group-item">......</li>`;
-}
+
 $(() => {
-    $('#link_click').click(() => {
-        $('.list-group').append(html.li)
+    $('button').click(function () {
+        alert("ok");
     });
 })
